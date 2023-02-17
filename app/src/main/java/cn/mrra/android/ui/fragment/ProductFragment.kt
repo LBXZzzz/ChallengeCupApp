ab859a30d37717d79df18a06ad56436a554906d9
LBXZzzz <1695677658@qq.com>
2023-02-17 21:47:29 +0800

    feat(app):navigation添加了产品以及产品fragment

---
+++ app/src/main/java/cn/mrra/android/ui/fragment/ProductFragment.kt
@@ -0,0 +1,22 @@
+package cn.mrra.android.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cn.mrra.android.common.base.BaseFragment
+import cn.mrra.android.databinding.FragmentMrraBinding
+import cn.mrra.android.databinding.FragmentProductBinding
+
+class ProductFragment : BaseFragment<FragmentProductBinding>() {
+    override var _binding: FragmentProductBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProductBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}

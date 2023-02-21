47b205d6a655a401d819c68758ff13e3153ab376
LBXZzzz <1695677658@qq.com>
2023-02-21 21:19:44 +0800

    feat(app):增加咨询activity

---
+++ app/src/main/java/cn/mrra/android/ui/activity/ConsultActivity.kt
@@ -0,0 +1,12 @@
+package cn.mrra.android.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.mrra.android.R
+
+class ConsultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consult)
+    }
+}

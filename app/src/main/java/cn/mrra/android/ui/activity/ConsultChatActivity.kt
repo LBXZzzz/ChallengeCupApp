51b18280d98bac6cd9473120b343038b7ceba260
蜡笔小朱搞安卓 <1695677658@qq.com>
2023-03-02 20:18:54 +0800

    feat(app):增加了咨询界面的中英文适配

--- app/src/main/java/cn/mrra/android/ui/activity/ConsultChatActivity.kt
+++ app/src/main/java/cn/mrra/android/ui/activity/ConsultChatActivity.kt
@@ -1,7 +1,10 @@
 package cn.mrra.android.ui.activity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import cn.mrra.android.R
 import cn.mrra.android.common.base.SimpleActivity
 import cn.mrra.android.databinding.ActivityConsultChatBinding
@@ -11,6 +14,11 @@ class ConsultChatActivity : SimpleActivity<ActivityConsultChatBinding>() {
     override val layoutId: Int = R.layout.activity_consult_chat
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        //通知栏沉浸式
+        window.statusBarColor = Color.TRANSPARENT
+        val lly = findViewById<LinearLayout>(R.id.lly_consult_chat)
+        lly.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
         binding.ivConsultChatReturn.setOnClickListener{finish()}
     }
 }

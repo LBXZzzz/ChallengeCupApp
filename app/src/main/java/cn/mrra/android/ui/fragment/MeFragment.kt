fec0022db6aa69c8067c6e97c3de4a37bee1a901
蜡笔小朱搞安卓 <1695677658@qq.com>
2023-03-08 14:48:37 +0800

    feat(app):美化了一部分界面

--- app/src/main/java/cn/mrra/android/ui/fragment/MeFragment.kt
+++ app/src/main/java/cn/mrra/android/ui/fragment/MeFragment.kt
@@ -1,6 +1,7 @@
 package cn.mrra.android.ui.fragment
 
 import android.os.Bundle
+import android.widget.Button
 import cn.mrra.android.R
 import cn.mrra.android.common.base.SimpleFragment
 import cn.mrra.android.common.startActivity
@@ -10,7 +11,7 @@ import cn.mrra.android.ui.activity.SettingsActivity
 class MeFragment : SimpleFragment<FragmentMeBinding>() {
 
     override val layoutId: Int = R.layout.fragment_me
-
+    
     override fun onFragmentCreated(savedInstanceState: Bundle?) {
         binding.tvMeSettings.setOnClickListener {
             startActivity<SettingsActivity>()
